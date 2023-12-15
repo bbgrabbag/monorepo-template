6214d8df955d64b3ea297476e4be700895f65049
Ben Turner <bbgrabbag@gmail.com>
2023-12-15 15:55:38 -0700

    format home page stories

--- apps/next-app-template/src/modules/home/components/HomePage/HomePage.stories.tsx
+++ apps/next-app-template/src/modules/home/components/HomePage/HomePage.stories.tsx
@@ -2,16 +2,15 @@ import * as React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { HomePage } from "./HomePage";
 
-
 const meta: Meta<typeof HomePage> = {
-    title: "Next App Template/Modules/Home",
-    component: HomePage,
-    args: {},
+  title: "Next App Template/Modules/Home",
+  component: HomePage,
+  args: {},
 };
 
 export default meta;
 type Story = StoryObj<typeof HomePage>;
 
 export const Default: Story = {
-    render: () => <HomePage />
+  render: () => <HomePage />,
 };
